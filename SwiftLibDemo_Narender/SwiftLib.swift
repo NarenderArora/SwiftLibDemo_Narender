50c0cc9537d6b9ea8cfa8b33426231953dcd5597
Narender Arora <a_Narender.Arora@airtelbank.com>
2023-07-14 18:50:37 +0530

    swift lib added

---
+++ SwiftLibDemo_Narender/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLibDemo_Narender
+//
+//  Created by A1TM1WZ7 on 14/07/23.
+//
+
+import Foundation
+
+public final class SwiftLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ SwiftLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLibExamples
+//
+//  Created by A1TM1WZ7 on 14/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
